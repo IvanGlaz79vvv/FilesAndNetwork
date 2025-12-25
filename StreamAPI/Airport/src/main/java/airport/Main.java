66fd458adde8d034221af48fa4a029b153b1f49d
ivan_glazunov <q-q@inbox.ru>
2025-12-25 05:34:21 +0300

    Сделал первые два метода: findCountAircraftWithModelAirbus//"Поиск количества самолетов модели Airbus" findMapCountParkedAircraftByTerminalName//Поиск количества припаркованных самолетов во всех терминалам

--- StreamAPI/Airport/src/main/java/airport/Main.java
+++ StreamAPI/Airport/src/main/java/airport/Main.java
@@ -2,23 +2,42 @@ package airport;
 
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
         //TODO Метод должен вернуть количество самолетов указанной модели.
         // подходят те самолеты, у которых name начинается со строки model
-        return 0;
+        return airport.getAllAircrafts().stream()
+                .filter(a -> a.getModel().startsWith(model)) // Фильтруем нужные самолёты
+                .count();                                   // Подсчитываем их количество
     }
 
     public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
         //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
-        return Collections.emptyMap();
+        Map<String,Integer> infoOfParkedAircraft = airport.getTerminals().stream()
+                .collect(Collectors.toMap(
+                        Terminal::getName,                   // Название терминала
+                        t -> t.getParkedAircrafts().size()    // Количество припаркованных самолётов
+                ));
+
+        Map<String, Integer> infoOfParkedAircraftSortedByName = infoOfParkedAircraft.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey()) // Сортировка по ключу (имени терминала)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> v1, // конфликт разрешаем первым значением (редко бывает актуально)
+                        LinkedHashMap::new // Поддерживаем порядок сортировки
+                ));
+
+        infoOfParkedAircraftSortedByName.forEach((terminal, aircraftCount) ->
+                System.out.println(terminal + ": " + aircraftCount));
+
+        return infoOfParkedAircraftSortedByName;
     }
 
     public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
